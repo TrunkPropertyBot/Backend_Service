2138f1205e2d6b2ee88fb876cd4fe98a2a2f4bf4
Elliott Gorrell <elliottgorrell1@gmail.com>
2018-04-17 16:02:04 +1000

    fixed missing set of message

--- src/routes/watson.js
+++ src/routes/watson.js
@@ -1,6 +1,6 @@
 module.exports = (app, watson) => {
   app.post('/message', (req, res) => {
-
+    const message = req.body.message;
     watson.assistant.message(message, (text) => {
       return res.json({output: {text: text}});
       });
